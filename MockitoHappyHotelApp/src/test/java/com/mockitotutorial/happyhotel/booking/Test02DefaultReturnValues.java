4a7855febd541f99e7fd7632ae4cbbb72ac2eff8
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-21 08:58:26 +0300

    explanations about return types

--- MockitoHappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test02DefaultReturnValues.java
+++ MockitoHappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test02DefaultReturnValues.java
@@ -25,6 +25,7 @@ class Test02DefaultReturnValues {
 		
 		this.bookingService = new BookingService(paymentServiceMock, roomServiceMock, bookingDAOMock, mailSenderMock);
 		
+		// Demonstrates default return values from mocks for different types:
 		System.out.println("List returned " + roomServiceMock.getAvailableRooms());
 		System.out.println("Object returned " + roomServiceMock.findAvailableRoomId(null));
 		System.out.println("Primitive returned " + roomServiceMock.getRoomCount());
@@ -36,6 +37,11 @@ class Test02DefaultReturnValues {
 		int expected = 0; 
 		
 		// When
+		// Mockito will return "nice mocks" by default. Values such as empty list, null, 0 and false primitives.
+		// 
+		// Below method calls getAvailableRooms() method which returns a list of type Room. 
+		// In this case it will return an empty list.
+		// This method will count 0 available rooms because of that reason. Therefore expected value is 0.
 		int actual = bookingService.getAvailablePlaceCount();
 		
 		// Then
