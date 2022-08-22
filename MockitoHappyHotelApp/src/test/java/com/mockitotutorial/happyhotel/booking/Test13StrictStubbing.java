8cb792419880a815eecbbfc32aad3251e58b4828
upperdim <45048352+upperdim@users.noreply.github.com>
2022-08-22 20:12:02 +0300

    strict stubbing and lenient

---
+++ MockitoHappyHotelApp/src/test/java/com/mockitotutorial/happyhotel/booking/Test13StrictStubbing.java
@@ -0,0 +1,62 @@
+package com.mockitotutorial.happyhotel.booking;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
+
+import java.time.LocalDate;
+import java.util.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class Test13StrictStubbing {
+
+	@InjectMocks
+	private BookingService bookingService;
+	
+	@Mock
+	private PaymentService paymentServiceMock;
+	
+	@Mock
+	private RoomService roomServiceMock;
+	
+	@Mock
+	private BookingDAO bookingDAOMock;
+	
+	@Mock
+	private MailSender mailSenderMock;
+	
+	@Captor
+	private ArgumentCaptor<Double> doubleCaptor;
+
+	// When we use BDD Mockito aliases, a feature called "Strict Stubbing" is automatically enabled.
+	@Test
+	void should_InvokePayment_when_Prepaid() {
+		// Given
+		BookingRequest bookingRequest = new BookingRequest("1", LocalDate.of(2020, 01, 01), LocalDate.of(2020, 01, 05), 2, false);
+		
+		// Booking is not prepaid but we still try to mock the payment service 
+		// which will be never used in this unit test because we are not going to invoke the payment service mock
+		// because bookingRequest is not prepaid and in the makeBooking() method, 
+		// if the bookingRequest is not prepaid, payment service is not invoked at all
+		//
+		// this will give the error "unnecessary stubbing"
+		//
+		// stubbing means defining behaviour with methods like when().thenReturn() etc.
+		//
+		// lenient(). ... method will supress this error
+		lenient().when(paymentServiceMock.pay(any(),  anyDouble())).thenReturn("1");
+		
+		// When
+		bookingService.makeBooking(bookingRequest);
+		
+		// Then
+		// No exception is thrown
+	}
+
+}
